3a449a816a0a0c21398c8e6dab086859972c8976
Jose Ojeda <joche.ojeda@bitframeworks.com>
2021-10-08 18:22:44 -0700

    service is working

---
+++ Examples/MAUI/SyncFrameworkApp.WebApp/Controllers/SyncController.cs
@@ -0,0 +1,67 @@
+﻿using BIT.Data.Sync;
+using BIT.Data.Sync.Server;
+
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace SyncFrameworkApp.WebApp.Controllers
+{
+    //[Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class SyncController : ControllerBase
+    {
+
+
+        private readonly ILogger<SyncController> _logger;
+        private readonly ISyncServer _SyncServer;
+        protected string GetHeader(string HeaderName)
+        {
+            return HttpContext.Request.Headers[HeaderName];
+        }
+
+
+
+        public SyncController(ILogger<SyncController> logger, ISyncServer SyncServer)
+        {
+
+            _logger = logger;
+            _SyncServer = SyncServer;
+        }
+        [HttpPost(nameof(Push))]
+        public virtual async Task Push([FromBody] List<Delta> deltas)
+        {
+            var Identity = deltas.FirstOrDefault().Identity;
+            var DeltaCount = deltas.Count;
+            string DeltaStoreName = GetHeader("DeltaStoreName");
+            string DeltaProcessorName = GetHeader("DeltaProcessorName");
+            var Message = string.Format("Push Delta Store:{0} Delta Processor: {1} Deltas Received: {2} Identity: {3}", DeltaStoreName, DeltaProcessorName, Identity, DeltaCount);
+            _logger.LogInformation(Message);
+
+
+            await _SyncServer.SaveDeltasAsync(deltas, DeltaStoreName, new CancellationToken());
+
+        }
+        [HttpGet("Fetch")]
+        public async Task<IEnumerable<IDelta>> Fetch(Guid startindex, string identity)
+        {
+            string name = GetHeader("DeltaStoreName");
+            _logger.LogInformation("Fetch Start Index:{0} Identity:{1}", startindex, identity);
+            IEnumerable<IDelta> enumerable = await _SyncServer.GetDeltasAsync(name, startindex, identity, new CancellationToken());
+            var Message = string.Format("Fetch invoked Identity:{0} Delta Store {1} Start Index:{2} Deltas returned:{3}  ", identity, name, startindex, enumerable.Count());
+            _logger.LogInformation(Message);
+
+
+
+            return enumerable;
+
+        }
+        [HttpGet()]
+        public string Get()
+        {
+            return "Is working )))";
+        }
+
+    }
+}
--- Examples/MAUI/SyncFrameworkApp.WebApp/Program.cs
+++ Examples/MAUI/SyncFrameworkApp.WebApp/Program.cs
@@ -1,3 +1,9 @@
+using BIT.Data.Sync.Options;
+using BIT.Data.Sync.Server;
+using BIT.Data.Sync.Server.Extensions;
+using BIT.EfCore.Sync;
+using BIT.EfCore.Sync.DeltaProcessors;
+using BIT.EfCore.Sync.DeltaStores;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using SyncFrameworkApp.WebApp.Data;
@@ -8,6 +14,35 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+
+//SYNC
+IConfigurationSection ConfigMemoryDeltaStore1 = builder.Configuration.GetSection("DeltaStore:MemoryDeltaStore1");
+IConfigurationSection ConfigMemoryDeltaStore2 = builder.Configuration.GetSection("DeltaStore:MemoryDeltaStore2");
+
+builder.Services.Configure<DeltaStoreSettings>("DemoApp", ConfigMemoryDeltaStore1);
+builder.Services.Configure<DeltaStoreSettings>("MemoryDeltaStore2", ConfigMemoryDeltaStore2);
+
+List<DeltaStoreConfigurationOptions> DeltaStores = new List<DeltaStoreConfigurationOptions>();
+DeltaStoreConfigurationOptions MemoryDeltaStore1 = new DeltaStoreConfigurationOptions(typeof(EFDeltaStore), "DemoApp");
+DeltaStoreConfigurationOptions MemoryDeltaStore2 = new DeltaStoreConfigurationOptions(typeof(MemoryDeltaStore), "MemoryDeltaStore2");
+
+DeltaStores.Add(MemoryDeltaStore1);
+DeltaStores.Add(MemoryDeltaStore2);
+
+List<DeltaStoreConfigurationOptions> DeltaProcessors = new List<DeltaStoreConfigurationOptions>();
+DeltaStoreConfigurationOptions MemoryDeltaProcessor1 = new DeltaStoreConfigurationOptions(typeof(MemoryDeltaProcessor), "MemoryDeltaStore1");
+DeltaStoreConfigurationOptions MemoryDeltaProcessor2 = new DeltaStoreConfigurationOptions(typeof(EFDeltaProcessor), "MemoryDeltaStore2");
+DeltaProcessors.Add(MemoryDeltaProcessor1);
+DeltaProcessors.Add(MemoryDeltaProcessor2);
+builder.Services.AddDataStoreTypes(DeltaStores.ToArray(), DeltaProcessors.ToArray());
+
+
+
+builder.Services.AddScoped<ISyncServer, SyncServerBase>();
+
+//SYNC
+
 
 var app = builder.Build();
 
@@ -28,5 +63,5 @@ app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-
+app.MapControllers();
 app.Run();
