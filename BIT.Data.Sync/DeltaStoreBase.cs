56e857bdf02ae927fa2b6a05180ffc0e7c43dcda
Jose Ojeda <joche.ojeda@bitframeworks.com>
2021-10-11 16:50:26 -0700

    cleanup

--- BIT.Data.Sync/DeltaStoreBase.cs
+++ BIT.Data.Sync/DeltaStoreBase.cs
@@ -29,7 +29,6 @@ namespace BIT.Data.Sync
         public abstract Task SaveDeltasAsync(IEnumerable<IDelta> deltas, CancellationToken cancellationToken = default);
 
         public abstract Task<IEnumerable<IDelta>> GetDeltasFromOtherNodes(Guid startindex, string identity, CancellationToken cancellationToken = default);
-
         public abstract Task<Guid> GetLastProcessedDeltaAsync(CancellationToken cancellationToken = default);
         public abstract Task SetLastProcessedDeltaAsync(Guid Index, CancellationToken cancellationToken = default);
 
