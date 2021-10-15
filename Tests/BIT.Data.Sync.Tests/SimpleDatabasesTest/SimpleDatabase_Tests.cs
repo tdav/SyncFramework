7054faac39158fd4a60b12003f8345d855497c0f
Jose Ojeda <joche.ojeda@bitframeworks.com>
2021-10-15 08:30:53 -0700

    Network cleanup

--- Tests/BIT.Data.Sync.Tests/SimpleDatabasesTest/SimpleDatabase_Tests.cs
+++ Tests/BIT.Data.Sync.Tests/SimpleDatabasesTest/SimpleDatabase_Tests.cs
@@ -21,59 +21,56 @@ namespace BIT.Data.Sync.Tests
             base.Setup();
         }
         [Test]
-        public async Task SyncMultipleClients_CRUD_Test()
+        public async Task SyncMultipleClients_CRUD_Test_Network_Test()
         {
             //0 - Get the network client connected to the API controller exposed by the test infrastructure
             var httpclient = this.GetTestClientFactory().CreateClient("TestClient");
             ISyncFrameworkClient syncFrameworkClient = new SyncFrameworkHttpClient(httpclient, "MemoryDeltaStore1");
 
-           
-
-            //2 - Create the master database
+            //1 - Create the master database
             SimpleDatabase Master = new SimpleDatabase("Master", syncFrameworkClient);
 
-            //4 - Create data and save it on the master
+            //2- Create data and save it on the master
             SimpleDatabaseRecord Hello = new SimpleDatabaseRecord() { Key = Guid.NewGuid(), Text = "Hello" };
             SimpleDatabaseRecord World = new SimpleDatabaseRecord() { Key = Guid.NewGuid(), Text = "World" };
             
             await Master.Add(Hello);
             await Master.Add(World);
 
-            //6 - Creating database A
+            //3- Creating database A
             SimpleDatabase A_Database = new SimpleDatabase("A", syncFrameworkClient);
             SimpleDatabaseDeltaProcessor A_DeltaProcessor = new SimpleDatabaseDeltaProcessor(A_Database.Data);
 
-            //7 - Create data and save it on database A
+            //4- Create data and save it on database A
             SimpleDatabaseRecord Hola = new SimpleDatabaseRecord() { Key = Guid.NewGuid(), Text = "Hola" };
             SimpleDatabaseRecord Mundo = new SimpleDatabaseRecord() { Key = Guid.NewGuid(), Text = "Mundo" };
             
             await A_Database.Add(Hola);
             await A_Database.Add(Mundo);
 
-           
 
-            //9 - Creating database B
+            //5 - Creating database B
             SimpleDatabase B_Database = new SimpleDatabase("B", syncFrameworkClient);
 
-            //10 - Create data and save it on database B
+            //6 - Create data and save it on database B
             SimpleDatabaseRecord Privet = new SimpleDatabaseRecord() { Key = Guid.NewGuid(), Text = "Privet" };
             SimpleDatabaseRecord Mir = new SimpleDatabaseRecord() { Key = Guid.NewGuid(), Text = "mir" };
             await B_Database.Add(Privet);
             await B_Database.Add(Mir);
 
 
-            //11 - Get deltas from all databases
+            //7 - Push deltas to the server
             await Master.PushAsync();
             await A_Database.PushAsync();
             await B_Database.PushAsync();
 
 
-            // Pull
+            //8 - Pull deltas from server
             await Master.PullAsync();
             await A_Database.PullAsync();
             await B_Database.PullAsync();
 
-            //15 - Write in the console the current state of each database
+            //9 - Write in the console the current state of each database
             Debug.WriteLine("Data in master");
             Master.Data.ForEach(r => Debug.WriteLine(r.ToString()));
             Debug.WriteLine("Data in master Last Processed Delta Index:" + await Master.DeltaStore.GetLastProcessedDeltaAsync(default));
@@ -89,7 +86,7 @@ namespace BIT.Data.Sync.Tests
             Debug.WriteLine("Data in B_Database Last Processed Delta Index:" + B_LastIndexProccesded);
 
 
-            //16 - Delete and update records in the master database
+            //10 - Delete and update records in the master database
             Master.Delete(Hola);
             Mundo.Text = "HOLA MUNDO";
             Master.Update(Mundo);
@@ -101,7 +98,7 @@ namespace BIT.Data.Sync.Tests
             Debug.WriteLine($"{System.Environment.NewLine}{System.Environment.NewLine}{System.Environment.NewLine}{System.Environment.NewLine}{System.Environment.NewLine}");
            
 
-            //18 - Write in the console the current state of each database
+            //11 - Write in the console the current state of each database
             Debug.WriteLine("Data in master");
             Master.Data.OrderBy(x => x.Key).ToList().ForEach(r => Debug.WriteLine(r.ToString()));
             Debug.WriteLine("Data in A_Database");
@@ -109,7 +106,7 @@ namespace BIT.Data.Sync.Tests
             Debug.WriteLine("Data in B_Database");
             B_Database.Data.OrderBy(x=>x.Key).ToList().ForEach(r => Debug.WriteLine(r.ToString()));
 
-            Debug.WriteLine("");
+          
 
 
 
@@ -118,7 +115,7 @@ namespace BIT.Data.Sync.Tests
             Assert.AreEqual(5, B_Database.Data.Count);
         }
         [Test]
-        public async Task SyncMultipleClients_CRUD_Network_Test()
+        public async Task SyncMultipleClients_CRUD()
         {
 
             //1 - Delta store for master database
